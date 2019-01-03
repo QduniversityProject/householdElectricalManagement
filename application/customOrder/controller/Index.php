f0b9a46fb6f94f54c8a09cf991fbba4bcdda1a11
Coulson <996714671@qq.com>
2019-01-03 14:50:49 +0800

    pdf初试成功

--- application/customOrder/controller/Index.php
+++ application/customOrder/controller/Index.php
@@ -24,7 +24,19 @@ class Index extends controller
    
     }
     public function test(){
-        echo json_decode("7777");
+        //获取员工编号
+    // $uid=$_COOKIE['u_id'];
+    //获取部门编号
+    // $pid=$_COOKIE['p_id'];
+    $pid=1;
+    $order=Db::table('custom_order')
+    ->alias('a')
+    ->join('buproduct b','a.product_num = b.product_rollno')
+    ->join('buemployee c','a.saler_num = c.employee_id')
+    ->where('c.department_id','=',($pid))
+    ->select();
+    $this->assign('cusTomOrder', $order);
+    return $this->fetch();
     }
 
     public function excelPrint()
@@ -105,6 +117,80 @@ class Index extends controller
         header('Content-Disposition: attachment;filename="订单.xlsx"');
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         $PHPWriter->save("php://output"); //表示在$path路径下面生成demo.xlsx文件
+    }
+
+    public function getPdf(){    
+// $html = <<<EOD
+// <h1>该功能正在维护--敬请期待</h1>  
+// EOD;
+            
+            // pdf($html);die;
+
+
+
+            function pdf($html=array(),$title="标题",$fileName=""){
+                $fileName = time();
+                vendor('tecnickcom.tcpdf.tcpdf'); //导入TCPDF类
+                $pdf = new \Tcpdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+                // 设置打印模式
+                //设置文件信息，头部的信息设置
+                $pdf->SetCreator(PDF_CREATOR);
+                $pdf->SetAuthor("作者");
+                $pdf->SetTitle($title);
+                $pdf->SetSubject('TCPDF Tutorial');
+                $pdf->SetKeywords('TCPDF, PDF, example, test, guide');//设置关键字 
+                // 是否显示页眉
+                $pdf->setPrintHeader(false);
+                // 设置页眉显示的内容
+                $pdf->SetHeaderData('logo.png', 60, 'baijunyao.com', '', array(0,64,255), array(0,64,128));
+                // 设置页眉字体
+                $pdf->setHeaderFont(Array('deja2vusans', '', '12'));
+                // 页眉距离顶部的距离
+                $pdf->SetHeaderMargin('5');
+                // 是否显示页脚
+                $pdf->setPrintFooter(true);
+                // 设置页脚显示的内容
+                $pdf->setFooterData(array(0,64,0), array(0,64,128));
+                // 设置页脚的字体
+                $pdf->setFooterFont(Array('dejavusans', '', '10'));
+                // 设置页脚距离底部的距离
+                $pdf->SetFooterMargin('10');
+                // 设置默认等宽字体
+                $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+                // 设置行高
+                $pdf->setCellHeightRatio(1);
+                // 设置左、上、右的间距
+                $pdf->SetMargins('10', '10', '10');
+                // 设置是否自动分页  距离底部多少距离时分页
+                $pdf->SetAutoPageBreak(TRUE, '15');
+                // 设置图像比例因子
+                $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+                if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+                    require_once(dirname(__FILE__).'/lang/eng.php');
+                    $pdf->setLanguageArray($l);
+                }
+                $pdf->setFontSubsetting(true);
+                $pdf->AddPage("A4","Landscape",true,true);
+                // 设置字体
+                $pdf->SetFont('stsongstdlight', '', 14, '', true);
+             
+                $pdf->writeHTML($html);//HTML生成PDF
+                //$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+                $showType= 'I';//PDF输出的方式。I，在浏览器中打开；D，以文件形式下载；F，保存到服务器中；S，以字符串形式输出；E：以邮件的附件输出。
+                 $pdf->Output("{$fileName}.pdf", $showType);
+             
+            }
+                //如果是把HTML页面转PDF格式执行以下代码
+                //   $html = "http://localhost/tp5/public/customOrder/index?page=1";
+                $html = "http://localhost/tp5/public/customOrder/index/test";
+                // $html="www.baidu.com";
+                  $data = file_get_contents($html);//获取html页面的url
+                //   dump($data);
+                  pdf($data);die;
+                 
+               
+               
+    
     }
     
 }
