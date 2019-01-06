dfc75744a66a4eaee4291c1657f0bd5c5ab3dddd
Leo <1243718816@qq.com>
2019-01-06 10:38:26 +0800

    1.6 第一次提交

--- application/product/controller/Productdetails.php
+++ application/product/controller/Productdetails.php
@@ -8,7 +8,7 @@ class Productdetails extends Controller
     public function productdetails($productdevelop_id=null)
     { 
         $list = Db::table('buproduct')
-        ->field('product_rollno,product_name,product_cost,product_price,product_color')
+        ->field('product_rollno,product_name,product_cost,product_price,product_color,productdevelop_id')
         ->where('productdevelop_id', "$productdevelop_id")
         ->select();
         $this->assign("pro",$list);
