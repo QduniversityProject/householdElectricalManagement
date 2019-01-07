2d8e2484cb399900a9e8bbcf1e9a639d51a2958c
shane <zgsdwangshang@163.com>
2019-01-07 16:29:42 +0800

    23232

--- application/afterSale/controller/Index.php
+++ application/afterSale/controller/Index.php
@@ -29,4 +29,44 @@ class Index extends controller
        
     }
 
+    public function vip($employee_id=null){
+        $info=Db::table('buaftersale')
+        ->alias('a')
+        ->join('custom_order b', 'a.order_num = b.order_num')
+        ->join('customer c', 'b.custom_num = c.customer_id')
+        ->where('employee_id', $employee_id)
+        ->where('c.level', '是')
+        ->order('a.state')
+        ->order('c.level desc')
+        ->paginate(5);
+        $this->assign('aftersaleInfo', $info);
+        return $this->fetch('Index/index_emp');
+    }
+    public function nofeedback($employee_id=null){
+        $info=Db::table('buaftersale')
+        ->alias('a')
+        ->join('custom_order b', 'a.order_num = b.order_num')
+        ->join('customer c', 'b.custom_num = c.customer_id')
+        ->where('employee_id', $employee_id)
+        ->where('a.state', '办理中')
+        ->order('a.state')
+        ->order('c.level desc')
+        ->paginate(5);
+        $this->assign('aftersaleInfo', $info);
+        return $this->fetch('Index/index_emp');
+    }
+    public function feedback($employee_id=null){
+        $info=Db::table('buaftersale')
+        ->alias('a')
+        ->join('custom_order b', 'a.order_num = b.order_num')
+        ->join('customer c', 'b.custom_num = c.customer_id')
+        ->where('employee_id', $employee_id)
+        ->where('a.state', '办结')
+        ->order('a.state')
+        ->order('c.level desc')
+        ->paginate(5);
+        $this->assign('aftersaleInfo', $info);
+        return $this->fetch('Index/index_emp');
+    }
+
 }
