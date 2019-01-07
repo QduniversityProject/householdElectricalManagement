75cc8edc690688d9abef360a62d9e5d1ecb2ce9d
Leo <1243718816@qq.com>
2019-01-07 11:13:41 +0800

    8888888

--- application/feedback/controller/Index.php
+++ application/feedback/controller/Index.php
@@ -10,17 +10,10 @@ class Index extends controller
     public function index($employee_id=null){
         if(isset($employee_id)){
             $info=Db::table('bufeedback')
-<<<<<<< HEAD
-        ->join('buaftersale','bufeedback.aftersale_roll = buaftersale.aftersale_roll')
-        ->paginate(5);
-        $this->assign('feedbackInfo', $info);
-           
-=======
             ->join('buaftersale','bufeedback.aftersale_roll = buaftersale.aftersale_roll')
             ->where('employee_id', $employee_id)
             ->paginate(5);
             $this->assign('feedbackInfo', $info);
->>>>>>> a9a22f027cb4addb4f8c832e4dcd0cf5c421cb57
             return $this->fetch('Index/index_emp');
         }
         else{
@@ -28,13 +21,8 @@ class Index extends controller
         ->join('buaftersale','bufeedback.aftersale_roll = buaftersale.aftersale_roll')
         ->paginate(5);
         $this->assign('feedbackInfo', $info);
-<<<<<<< HEAD
-            return $this->fetch('Index/index');
-        }
-=======
         return $this->fetch('Index/index');
         }
         
->>>>>>> a9a22f027cb4addb4f8c832e4dcd0cf5c421cb57
     }
 }
